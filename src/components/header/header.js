514e443e06a069549b24fbb41427e44ed6eacd50
camilaleite <camilalenas_@hotmail.com>
2022-06-20 21:16:27 +0044

    colocando imagem na header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,16 +1,28 @@
 import React from "react"
 import styled from "styled-components"
 import navBar from "./navBar"
+import Background from "../assets/desktop/backgroundheader.png"
+import { createGlobalStyle } from "styled-components"
+
 const Header = styled.header`
     width: 98%;
-    height: 80vh;
+    height: 98vh;
     margin: 15px auto;
-    background: #cecece;
-    background-image: url("../assets/desktop/backgroundheader.png");
+    border: solid;
+    background-image: url(${Background});
+    background-size: contain;
 `
+const GlobalStyle = createGlobalStyle `
+*{margin: 0;
+padding: 0;
+box-sizing:border-box;
+}
+`
+
 export default function HeaderComponent (){
     return(
         <Header>
+            <GlobalStyle/>
             {navBar()}
         </Header>
     )
